10cd2eea0083e9aba1a89ca40588d695a8a61cf5
Tian Yunhao <t123yh@outlook.com>
2021-11-26 14:15:36 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -6,4 +6,7 @@
 # -fsdb: 输出 fsdb 波形文件需要指定此选项
 # -kdb: 生成源代码数据库文件，供 Verdi 显示源代码使用
 # -q: 减少日志数量（安静模式）
-vcs -full64 src/*.v -o sim/simv -fsdb -kdb -q
+# -sverilog: 支持 SystemVerilog 语法
+# +incdir+src: 在 src 里面寻找 include 文件
+# -override_timescale=1ns/1ps: 强制将 timescale 设为 1ns/1ps
+vcs -full64 src/*.v -o sim/simv -fsdb -kdb -q +incdir+src -sverilog -override_timescale=1ns/1ps
